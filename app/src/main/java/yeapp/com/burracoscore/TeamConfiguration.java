5f301dd0faa5edb3518fb4e136211f94262f7540
powettino <iacoperi@gmail.com>
2015-01-16 03:15:02 +0100

    aggiunto tutti i land risolto un bug sulla gestioen del bottone salva nella conf risolto il layout non floating

--- app/src/main/java/yeapp/com/burracoscore/TeamConfiguration.java
+++ app/src/main/java/yeapp/com/burracoscore/TeamConfiguration.java
@@ -3,6 +3,7 @@ package yeapp.com.burracoscore;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.Menu;
@@ -10,7 +11,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import static android.view.View.OnClickListener;
 
@@ -21,8 +21,6 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
     private EditText gioc12Name;
     private EditText gioc21Name;
     private EditText gioc22Name;
-    private TextView team1Label;
-    private TextView team2Label;
     int numberOfPlayerForTeam = 0;
 
 //    final String varGioc12State = "gioc12Name";
@@ -43,12 +41,12 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
             }
             case R.id.singleConf: {
                 numberOfPlayerForTeam = 1;
-                changePlayers();
+                changeVisualConfigurationByPlayers();
                 break;
             }
             case R.id.coupleConf: {
                 numberOfPlayerForTeam = 2;
-                changePlayers();
+                changeVisualConfigurationByPlayers();
                 break;
             }
             case R.id.salvaConf: {
@@ -104,8 +102,6 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
         gioc12Name = (EditText) findViewById(R.id.giocatore12);
         gioc21Name = (EditText) findViewById(R.id.giocatore21);
         gioc22Name = (EditText) findViewById(R.id.giocatore22);
-        team1Label = (TextView) findViewById(R.id.teamAConf);
-        team2Label = (TextView) findViewById(R.id.teamBConf);
 
         gioc11Name.addTextChangedListener(this);
         gioc12Name.addTextChangedListener(this);
@@ -124,7 +120,7 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
                 gioc12Name.setText(startingIntent.getStringExtra(getString(R.string.nomeGiocatore12)));
                 gioc22Name.setText(startingIntent.getStringExtra(getString(R.string.nomeGiocatore22)));
             }
-            changePlayers();
+            changeVisualConfigurationByPlayers();
         } else {
             resetNames();
         }
@@ -133,6 +129,10 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putInt(getString(R.string.numberPlayerActivity), numberOfPlayerForTeam);
+        outState.putString(getString(R.string.nomeGiocatore11), gioc11Name.getText().toString());
+        outState.putString(getString(R.string.nomeGiocatore12), gioc12Name.getText().toString());
+        outState.putString(getString(R.string.nomeGiocatore21), gioc21Name.getText().toString());
+        outState.putString(getString(R.string.nomeGiocatore22), gioc22Name.getText().toString());
         super.onSaveInstanceState(outState);
     }
 
@@ -140,34 +140,31 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         numberOfPlayerForTeam = savedInstanceState.getInt(getString(R.string.numberPlayerActivity));
+        if (numberOfPlayerForTeam != 0) {
+            gioc11Name.setText(savedInstanceState.getString(getString(R.string.nomeGiocatore11)));
+            gioc21Name.setText(savedInstanceState.getString(getString(R.string.nomeGiocatore21)));
+            if (numberOfPlayerForTeam == 2) {
+                gioc12Name.setText(savedInstanceState.getString(getString(R.string.nomeGiocatore12)));
+                gioc22Name.setText(savedInstanceState.getString(getString(R.string.nomeGiocatore22)));
+            }
+            changeVisualConfigurationByPlayers();
+        } else {
+            resetNames();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_team_configuration, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-//        switch (item.getItemId()) {
-//
-//            case (R.id.chiudiMenuConf): {
-//                setResult(RESULT_CANCELED);
-//                finish();
-//                return true;
-//            }
-//            case R.id.salvaConf: {
-//                salvaNomi();
-//            }
-//            default: {
         return super.onOptionsItemSelected(item);
-//            }
-//        }
     }
 
-    private void changePlayers() {
+    private void changeVisualConfigurationByPlayers() {
         if (gioc11Name.getText().length() == 0) {
             gioc11Name.setText(R.string.nomeGiocatore11);
         }
@@ -179,10 +176,6 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
         }
         if (gioc22Name.getText().length() == 0) {
             gioc22Name.setText(R.string.nomeGiocatore22);
-        }
-        if (!team1Label.isEnabled() && !team2Label.isEnabled()) {
-            team2Label.setEnabled(true);
-            team1Label.setEnabled(true);
         }
         if (!salvaButton.isEnabled()) {
             salvaButton.setEnabled(true);
@@ -221,7 +214,7 @@ public class TeamConfiguration extends Activity implements OnClickListener, Text
             if (salvaButton.isEnabled())
                 salvaButton.setEnabled(false);
         } else {
-            if (!salvaButton.isEnabled()) {
+            if (!salvaButton.isEnabled() && numberOfPlayerForTeam != 0) {
                 salvaButton.setEnabled(true);
             }
         }
