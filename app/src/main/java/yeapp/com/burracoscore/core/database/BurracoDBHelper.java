9a5858b469c10b88aba4dc731ebb228db8078750
powettino <iacoperi@gmail.com>
2015-03-28 15:57:54 +0100

    Versione non funzionante per ANDREA

--- app/src/main/java/yeapp/com/burracoscore/core/database/BurracoDBHelper.java
+++ app/src/main/java/yeapp/com/burracoscore/core/database/BurracoDBHelper.java
@@ -5,7 +5,24 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import yeapp.com.burracoscore.core.database.columns.HelperConstants;
+
 public class BurracoDBHelper extends SQLiteOpenHelper {
+
+ // codice SQL per create la tabella
+//    private final static String CREATE_TABLE =
+//            "CREATE TABLE " + WiFiNetEntry.TABLE_NAME + " (" +
+//                    WiFiNetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+//                    WiFiNetEntry.COLUMN_NAME_TIMESTAMP + " INTEGER, " +
+//                    WiFiNetEntry.COLUMN_NAME_BSSID + " TEXT, " +
+//                    WiFiNetEntry.COLUMN_NAME_SSID + " TEXT, " +
+//                    WiFiNetEntry.COLUMN_NAME_SIGNAL + " INTEGER " +
+//                    ")";
+
+    // codice SQL per eliminare la tabella
+//    private final static String DROP_TABLE =
+//            "DROP TABLE IF EXISTS " + WiFiNetEntry.TABLE_NAME;
+
 
 //
 //    private final static String CREATE_TABLE = "CREATE TABLE "+ DBColumns.TABLE_NAME+"("
@@ -18,19 +35,25 @@ public class BurracoDBHelper extends SQLiteOpenHelper {
 //    private final static String DROP_TABLE = "DROP TABLE IF EXIST"+ DBColumns.TABLE_NAME;
 
     public BurracoDBHelper(Context context) {
-        super(context, "", null, 1);
+        super(context, HelperConstants.DBName, null, HelperConstants.DBVersion);
 //        super(context, DBHelperConstant.DBName, null, DBHelperConstant.DBVersion);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-//        db.execSQL(CREATE_TABLE);
-//        Log.d(DBHelperConstant.DBTag, "Creato");
+        db.execSQL(CREATE_TABLE);
+        Log.d(HelperConstants.DBTag, "Creato");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-//        db.execSQL(DROP_TABLE);
+        db.execSQL(DROP_TABLE);
         onCreate(db);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // idem come per onUpgrade
+        onUpgrade(db, oldVersion, newVersion);
     }
 }
--- app/src/main/java/yeapp/com/burracoscore/core/database/columns/GameColumns.java
+++ app/src/main/java/yeapp/com/burracoscore/core/database/columns/GameColumns.java
@@ -9,4 +9,6 @@ public class GameColumns implements BaseColumns {
     public static final String TOTALE_A = "totale_partita_a";
     public static final String TOTALE_B = "totale_partita_b";
     public static final String NUMERO_MANI = "numero_mani";
+    public static final String NUMERO_PARTITA = "numero_partita";
+    public static final String VINCITORE = "vincitore";
 }
